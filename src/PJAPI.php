2d2407baf4d23cf57a134fc41adb107b26fda3de
Etienne Bagnoud <etienne@artisan-numerique.ch>
2025-02-04 13:47:01 +0100

    Send chunk, renaming, print stream

--- src/PJAPI.php
+++ src/PJAPI.php
@@ -62,7 +62,11 @@ class PJAPIIterator implements Iterator {
         $reflection = $reflection->getMethod($operation);
         $neededArgs = $reflection->getParameters();
         foreach ($neededArgs as $arg) {
-            $help->args = ['name' => $arg->name, 'type' => strval($arg->getType()), 'optional' => $arg->isOptional()];
+            $help->args = [
+                'name' => $arg->name,
+                'type' => strval($arg->getType()),
+                'optional' => $arg->isOptional()
+            ];
         }
         $help->return = strval($reflection->getReturnType());
         return [$this->keys[$this->step], $help];
@@ -173,10 +177,18 @@ class PJAPIIterator implements Iterator {
 }
 
 class PJAPI {
+    /* as it's converted to base64 use multiple of 3 to avoid padding. The 
+     * default pad char is = which has meaning in HTTP, even if it poses no 
+     * problem to have = char in boundary.
+     */
+    const BOUNDARY_RAND_BYTES = 9;
+    const CHUNK_SIZE = 4096;
     protected Request $request;
     protected string $routeDirectory;
     protected PJAPIIterator $iterator;
     private string $boundary;
+    private array $partbuffer;
+    private int $partbuffer_count;
 
     public function __construct(string $routeDirectory) {
         if (is_dir($routeDirectory) === false) {
@@ -186,8 +198,77 @@ class PJAPI {
             throw new Exception('Invalid route directory', ERR_BAD_REQUEST);
         }
         $this->routeDirectory = $routeDirectory;
-        $this->boundary = 'PJAPI' . md5(uniqid());
+        /* According to my testing on my machine :
+         * - random_bytes is faster than uniqid
+         * - for some reason, random_bytes run x2 faster on PHP ZTS than non-ZTS
+         * PHP.
+         * - for some reason, base64_encode is a bit faster than bin2hex.
+         */
+        $this->boundary = 'PJAPI-' . base64_encode(random_bytes(self::BOUNDARY_RAND_BYTES));
+        $this->partbuffer = [];
+        $this->partbuffer_count = 0;
+    }
 
+    public function printf(int $stream, string $string, ...$format)
+    {
+        $this->print($stream, vsprintf($string, $format));
+    }
+
+    public function print (int $stream, string $string)
+    {
+        if (!isset($this->partbuffer[$stream])) { return false; }
+        $this->partbuffer[$stream] .= $string;
+        if (strlen($this->partbuffer[$stream]) >= self::CHUNK_SIZE) {
+            $this->flushStream($stream);
+        }
+        return true;
+    }
+
+    public function flushStream(int $stream) {
+        if (!isset($this->partbuffer[$stream])) { return; }
+        echo $this->partbuffer[$stream];
+        $this->partbuffer[$stream] = '';
+        flush();
+    }
+
+    protected function emitError (int $stream, Throwable $e) {
+        $this->printf($stream, '{"error":true, "message": %s, "version": %d, "code": %d}', json_encode($e->getMessage()), JAPI_VERSION, $e->getCode());
+        error_log($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine() . PHP_EOL . $e->getTraceAsString());
+        for ($e = $e->getPrevious(); $e !== null; $e = $e->getPrevious()) {
+            error_log($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine() . PHP_EOL . $e->getTraceAsString());
+        }
+    }
+
+    public function openStream($type = 'application/json; charset=utf-8'): int
+    {
+        $current_part = $this->partbuffer_count++;
+        $this->partbuffer[$current_part] = 
+            "--" . $this->boundary . "\r\n"
+            . 'Content-Type: ' . $type . "\r\n\r\n";
+        
+        return $current_part;
+    }
+
+    public function openTextStream():int {
+        return $this->openStream('plain/text; charset=utf-8');
+    }
+
+    public function openJsonStream():int {
+        return $this->openStream('application/json; charset=utf-8');
+    }
+
+    public function closeStream(int $stream) {
+        if (!isset($this->partbuffer[$stream])) { return; }
+        $this->partbuffer[$stream] .= "\r\n";
+        $this->flushStream($stream);
+        unset($this->partbuffer[$stream]);
+    }
+
+    public function closeRequest()
+    {
+        echo '--' . $this->boundary . "--\r\n";
+        flush();
+        fastcgi_finish_request();
     }
 
     public function init () {
@@ -197,95 +278,79 @@ class PJAPI {
             $payload = $this->request->getPayload();
             $this->iterator = new PJAPIIterator($this->routeDirectory, $payload, $this);
         } catch (Exception $e) {
-            $this->startPart(true);
-            $this->emitError($e);
-            $this->endPart();
+            $stream = $this->openJsonStream();
+            $this->emitError($stream, $e);
+            $this->closeStream($stream);
+            $this->closeRequest($stream);
             exit;
         }
     }
 
-    protected function emitError (Throwable $e) {
-        printf('{"error":true, "message": %s, "version": %d, "code": %d}', json_encode($e->getMessage()), JAPI_VERSION, $e->getCode());
-        error_log($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine() . PHP_EOL . $e->getTraceAsString());
-        for ($e = $e->getPrevious(); $e !== null; $e = $e->getPrevious()) {
-            error_log($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine() . PHP_EOL . $e->getTraceAsString());
-        }
-    }
-
-    public function startPart (bool $last = false) {
-        echo '--' . $this->boundary . ($last ? "--\r\n" : "\r\n") ;
-        if ($last) { return; }
-        echo 'Content-Type: application/json; charset=utf-8' . "\r\n";
-        echo "\r\n";
-    }
-
-    public function endPart () {
-        echo "\r\n";
-    }
-
     public function run () {
-            $success = 0;
-            $error = 0;
-            try {
-                foreach ($this->iterator as $item) {
-                    if ($item[1] instanceof Throwable) {
-                        $error++;
-                        $this->startPart();
-                        echo '{"' . $item[0] . '":';
-                        $this->emitError($item[1]);
-                        echo '}';
-                        $this->endPart();
+        $success = 0;
+        $error = 0;
+        try {
+            foreach ($this->iterator as $item) {
+                if ($item[1] instanceof Throwable) {
+                    $error++;
+                    $streamid = $this->openJsonStream();
+                    $this->print($streamid, '{"' . $item[0] . '":');
+                    $this->emitError($streamid, $item[1]);
+                    $this->print($streamid, '}');
+                    $this->closeStream($streamid);
+                    continue;
+                }
+                if ($item[1] instanceof Generator 
+                        || $item[1] instanceof Iterator
+                        || is_array($item[1])) {
+                    if (is_array($item[1])) {
+                        $item[1] = new ArrayIterator($item[1]);
+                    }
+                    if ($item[1]->valid()) {
+                        $streamid = $this->openJsonStream();
+                        $this->printf($streamid, '{"%s": {"error": false, "result": [', $item[0]);
+                        do {
+                            $doPrint = true;
+                            /* in case of error in a single item, we skip it */
+                            $encoded = json_encode($item[1]->current());
+                            if ($encoded === false) {
+                                $item[1]->next();
+                                error_log('Invalid JSON encoding ' . json_last_error_msg());
+                                $doPrint = false;
+                                continue;
+                            }
+                            $this->print($streamid, $encoded);
+                            $item[1]->next();
+                        } while($item[1]->valid() && ($doPrint ? $this->print($streamid, ',') : true));
+                        $this->print($streamid, ']}}');
+                        $this->closeStream($streamid);
+                    } else {
+                        $streamid = $this->openJsonStream();
+                        $this->printf($streamid, '{"%s": {"error": false, "result": []}}', $item[0]);
+                        $this->closeStream($streamid);
+                    }
+                } else {
+                    $streamid = $this->openJsonStream();
+                    $encoded = json_encode($item[1]);
+                    if ($encoded === false) {
+                        error_log('Invalid JSON encoding ' . json_last_error_msg());
+                        $this->print($streamid, '{"' . $item[0] . '":');
+                        $this->emitError($streamid, new Exception('Invalid JSON encoding', ERR_INTERNAL));
+                        $this->print($streamid, '}');
+                        $this->closeStream($streamid);
                         continue;
                     }
-                    if ($item[1] instanceof Generator 
-                            || $item[1] instanceof Iterator
-                            || is_array($item[1])) {
-                        if (is_array($item[1])) {
-                            $item[1] = new ArrayIterator($item[1]);
-                        }
-                        if ($item[1]->valid()) {
-                            $this->startPart();
-                            printf('{"%s": {"error": false, "result": [', $item[0]);    
-                            do {
-                                $doPrint = true;
-                                /* in case of error in a single item, we skip it */
-                                $encoded = json_encode($item[1]->current());
-                                if ($encoded === false) {
-                                    $item[1]->next();
-                                    error_log('Invalid JSON encoding ' . json_last_error_msg());
-                                    $doPrint = false;
-                                    continue;
-                                }
-                                print($encoded);
-                                $item[1]->next();
-                            } while($item[1]->valid() && ($doPrint ? print(',') : true));
-                            echo ']}}';
-                            $this->endPart();
-                        } else {
-                            $this->startPart();
-                            printf('{"%s": {"error": false, "result": []}}', $item[0]);
-                            $this->endPart();
-                        }
-                    } else {
-                        $this->startPart();
-                        $encoded = json_encode($item[1]);
-                        if ($encoded === false) {
-                            error_log('Invalid JSON encoding ' . json_last_error_msg());
-                            $this->emitError(new Exception('Invalid JSON encoding', ERR_INTERNAL));
-                            $this->endPart();
-                            continue;
-                        }
 
-                        printf('{"%s": {"error": false, "result": %s}}', $item[0], $encoded);
-                        $this->endPart();
-                    }
-                    $success++;
+                    $this->printf($streamid, '{"%s": {"error": false, "result": %s}}', $item[0], $encoded);
+                    $this->closeStream($streamid);
                 }
-            } catch (Throwable $e) {
-                $this->startPart();
-                $this->emitError($e);
-                $this->endPart();
+                $success++;
             }
-            $this->startPart(true);
+        } catch (Throwable $e) {
+            $streamid = $this->openJsonStream();
+            $this->emitError($streamid, $e);
+            $this->closeStream($streamid);
         }
+        $this->closeRequest();
+    }
 }
